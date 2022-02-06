b84821aa1511e47a23b79324a642525cb58ca8de
Murilo Vargas <muriraubach@gmail.com>
2022-02-06 16:52:54 -0300

    chore: update routes.ts imports

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
-import { CreateClientController } from "./modules/clients/useCases/createClient/createClientController";
+import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 
 const routes = Router()
 
