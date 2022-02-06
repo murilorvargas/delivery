55f0e5b9abb8d9c342be6cb20e65f8893fba0d43
Murilo Vargas <muriraubach@gmail.com>
2022-02-06 15:48:33 -0300

    feat: add error handling in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors"
 
 import { routes } from "./routes";
 
@@ -7,5 +8,17 @@ const app = express();
 app.use(express.json())
 
 app.use(routes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    })
+  }
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 app.listen(3000, () => console.log("server is running!"));
