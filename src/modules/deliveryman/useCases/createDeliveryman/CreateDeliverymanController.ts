08d0e280835fdb5fd55ae4acda619e92ce0b3daa
Murilo Vargas <muriraubach@gmail.com>
2022-02-06 16:58:55 -0300

    feat: add create deliveryman controller

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import { CreateDeliverymanUseCase } from "./CreateDeliverymanUseCase";
+
+export class CreateDeliverymanController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { username, password } = req.body
+
+    const createDeliverymanUseCase = new CreateDeliverymanUseCase();
+
+    const result = await createDeliverymanUseCase.execute({
+      username,
+      password
+    })
+
+
+    return res.json(result)
+  }
+}
