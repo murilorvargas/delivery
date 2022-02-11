cc05bcce443bb199415098519bc0d49513e1c878
Murilo Vargas <muriraubach@gmail.com>
2022-02-11 19:35:21 -0300

    refactor: rename find all deliveries use case to find all deliveries client use case in find all deliveries client controller

--- src/modules/clients/useCases/findAllDeliveries/FindAllDeliveriesClientController.ts
+++ src/modules/clients/useCases/findAllDeliveries/FindAllDeliveriesClientController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
+
 import { FindAllDeliveriesClientUseCase } from "./FindAllDeliveriesClientUseCase";
 
 export class FindAllDeliveriesClientController {
   async handle(req: Request, res: Response) {
     const { id_client } = req 
 
-    const findAllDeliveriesUseCase = new FindAllDeliveriesClientUseCase()
-    const deliveries = await findAllDeliveriesUseCase.execute(id_client)
+    const findAllDeliveriesClientUseCase = new FindAllDeliveriesClientUseCase()
+    const deliveries = await findAllDeliveriesClientUseCase.execute(id_client)
 
     return res.json(deliveries)
   }
